1a37f7978e8ce240759dcc27b85375fab3b2d242
Khanh Huynh <khanh.huynh@axonactive.com>
2020-03-07 22:18:34 +0700

    first commit

---
+++ src/main/java/ch/klara/hello_world/HelloResource.java
@@ -0,0 +1,14 @@
+package ch.klara.hello_world;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("hellos")
+public class HelloResource {
+
+	@GET
+	public Response sayHello() {
+		return Response.ok("Hello world Hallo!").build();
+	}
+}
